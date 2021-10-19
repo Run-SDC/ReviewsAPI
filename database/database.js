03d91fa308a448e5a81db9838416740438506ccc
Josh Kohl <joshdkohl@gmail.com>
2021-10-18 22:28:59 -0400

    [WIP] Sequlize and Postgres setup

---
+++ database/database.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import { Sequelize, Model, Datatypes } from 'sequelize';
+import Credentials from '../credentials';
+
+// const user = 'josh';
+// const host = 'localhost';
+// const database = '<Reviews>';
+// const password = Credentials.dbPassword;
+// const port = '8000';
+
+// const sequelize = new Sequelize(database, user, password, {
+//   host,
+//   port,
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('Reviews', 'josh', {
+  host: 'localhost',
+  dialect: 'postgres',
+  password: '',
+  dialectOptions: {
+    multipleStatements: true,
+  },
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
