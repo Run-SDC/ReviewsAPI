b93cf404d05bc2252ae9da72c8fcf542c7816ec5
Josh Kohl <joshdkohl@gmail.com>
2021-10-27 18:51:35 -0400

    Stubbbed out routes in server file

--- server/server.js
+++ server/server.js
@@ -15,6 +15,27 @@ app.get('/', (req, res) => {
   res.json('Server is running...');
 });
 
+
+app.get('/reviews' (req, res) => {
+  //helper function from db to pull reviews for a given product
+})
+
+app.get('/meta' (req, res) => {
+  //helper function from db to pull meta data for a products reviews
+})
+
+app.post('/reviews' (req, res) => {
+  //helper function from db to add new entry
+})
+
+app.put('/helpful' (req, res) => {
+  //helper function from db to update entry helpfulness field
+})
+
+app.put('/report' (req, res) => {
+  //helper function from db to flag review
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
